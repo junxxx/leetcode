403e8e836d3c0ac6b0ed4c3bca0e0be6aba579b1
junxxx <hprjunxxx@gmail.com>
2018-12-10 14:15:02 +0800

    optimize array

--- array/easy/remove_duplicates_from_sorted_array.c
+++ array/easy/remove_duplicates_from_sorted_array.c
@@ -6,34 +6,17 @@
  *
  * Do not allocate extra space for another array, 
  * you must do this by modifying the input array in-place with O(1) extra memory.
- * version: 1.0  faster than 7.47% of C online submissions
+ * version: 2.0  faster than 100.00% of C online submissions
  *
  */
 int removeDuplicates(int* nums, int numsSize) {
-    int len = numsSize;
-    for(int i = 0; i < len; i++) {
-        for(int j = i + 1; j < len;j++ ) {
-            if(nums[j] == nums[i]) {
-                //found same element
-                int same = 1;
-                for(int shift = j + 1; shift < len;shift++) {
-                    if(nums[shift] == nums[i]) {
-                        same++;
-                    }
-                    else
-                        break;
-                }
-                for(int index = j; index < len - same; index++) {
-                    nums[index] = nums[index+same];
-                }
-                len = len - same;
-            }
-            else
-                break;
+    int i = 0;
+    for (int j = 1; j < numsSize; j++) {
+        if(nums[j] != nums[i]) {
+            nums[++i] = nums[j];
         }
     }
-
-    return len;
+   return i + 1;
 }
 
 void printf_arr(int *arr, int num) {
